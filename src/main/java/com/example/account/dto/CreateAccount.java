b9cf565b5ada04953b460b769fa9d3b8a139766e
jungheol <jungheol@naver.com>
2024-06-15 14:45:20 +0900

    새로운 계좌 생성 및 저장

--- src/main/java/com/example/account/dto/CreateAccount.java
+++ src/main/java/com/example/account/dto/CreateAccount.java
@@ -2,13 +2,20 @@ package com.example.account.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public class CreateAccount {
     @Getter
     @Setter
     public static class Request {
+        @NotNull
+        @Min(1)
         private Long userId;
+
+        @NotNull
+        @Min(100)
         private Long initialBalance;
     }
 
--- src/main/java/com/example/account/service/AccountService.java
+++ src/main/java/com/example/account/service/AccountService.java
@@ -1,25 +1,51 @@
 package com.example.account.service;
 
 import com.example.account.domain.Account;
-import com.example.account.domain.AccountStatus;
+import com.example.account.domain.AccountUser;
+import com.example.account.exception.AccountException;
 import com.example.account.repository.AccountRepository;
+import com.example.account.repository.AccountUserRepository;
+import com.example.account.type.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
+import java.time.LocalDateTime;
+
+import static com.example.account.type.AccountStatus.IN_USE;
+
 @Service
 @RequiredArgsConstructor
 public class AccountService {
     private final AccountRepository accountRepository;
+    private final AccountUserRepository accountUserRepository;
 
+    /**
+     * 사용자가 있는지 확인 조회
+     * 계좌 번호 생성
+     * 계좌 저장, 정보 토스
+     */
     @Transactional
-    public void createAccount() {
-        Account account = Account.builder()
-                .accountNumber("40000")
-                .accountStatus(AccountStatus.IN_USE)
-                .build();
-        accountRepository.save(account);
+    public Account createAccount(Long userId, Long initialBalance) {
+        // 사용자 정보 찾아오기. 없다면 에러 코드 표시
+        AccountUser accountUser = accountUserRepository.findById(userId)
+                .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+
+        // db에 계좌가 있다면 계좌번호에 +1 해서 새로운 계좌 생성, 없다면 10000000000 으로 계좌 생성
+        String newAccountNumber = accountRepository.findFirstByOrderByIdDesc()
+                  .map(account -> (Integer.parseInt(account.getAccountNumber())) + 1 + "")
+                  .orElse("1000000000");
+
+        return accountRepository.save(
+                Account.builder()
+                      .accountUser(accountUser)
+                      .accountStatus(IN_USE)
+                      .accountNumber(newAccountNumber)
+                      .balance(initialBalance)
+                      .registeredAt(LocalDateTime.now())
+                      .build()
+        );
     }
 
     @Transactional
